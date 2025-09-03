4778e1b2f358c57f1a507acb45373afb80fe781f
jfoster2995-stack <jfoster2995@yahoo.com>
2025-09-03 18:27:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace "your-username" and "repo-name" with your actual repo details
 export default defineConfig({
   plugins: [react()],
+  base: "/32-team-trade-calculator/",  
 })
